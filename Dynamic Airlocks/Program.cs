52d43cdf2f0db437e974076bc6952982a2e74330
caithford <46058242+caithford@users.noreply.github.com>
2019-01-04 22:24:00 -0700

    Upload 2

---
+++ Dynamic Airlocks/Program.cs
@@ -0,0 +1,154 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        bool initialized = false;
+        int initmode = 0;
+        List<AirlockType> airlockList = new List<AirlockType>();
+        Dictionary<int, string> airlockInventory = new Dictionary<int, string>();
+
+        //AirlockClass airlock;
+
+        IMyTextPanel debugLCD = null;
+
+        public Program()
+        {
+            airlockList.Clear();
+
+            Runtime.UpdateFrequency = UpdateFrequency.Update100; // set PB to run every 100 ticks
+
+            debugLCD = GridTerminalSystem.GetBlockWithName("Airlock Debug LCD") as IMyTextPanel;
+
+            if (debugLCD == null)
+            {
+                throw new Exception("debugLCD is not assigned, please name LCD block 'Airlock Debug LCD'.");
+            }
+        }
+
+        public void Save()
+        {
+        }
+
+        public void Main(string argument)
+        {
+
+
+            /* 
+             * initialize 1: get all air vent blocks. condense to list of airlock master vents
+             * initialize 2: get all LCD blocks. This is for debug output.
+             * initialize 3: get all door blocks. put doors in appropriate airlocks
+             * initialize 4: lcd's
+             * initialize 5: lights
+             * initialize 6: sounds
+             * 
+             */
+
+            if (!initialized)
+            { Initialize(); } // run init
+        }
+
+        private void Initialize ()
+        {
+            switch (initmode)
+            {
+                case 0: Init_Phase1(); break;
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+                case 5: break;
+                case 6: break;
+            }
+            initmode++;
+        }
+
+        private void Init_Phase1 ()
+        {
+            List<IMyTerminalBlock> blocklist = new List<IMyTerminalBlock>();
+            AirlockType airlock;
+
+            int i = 0;
+            int id = 0;
+            string s = "";
+            string[] commands;
+            string[] values;
+
+            Echo("Searching for Airlocks...");
+
+            blocklist.Clear();
+            GridTerminalSystem.GetBlocksOfType<IMyAirVent>(blocklist);
+
+            for (i = 0; i < blocklist.Count; i++)
+            {
+                commands = blocklist[i].CustomData.Split(';');
+
+                foreach (string command in commands)
+                {
+                    if (command.StartsWith("Airlock:"))
+                    {
+                        s = command.Remove(0, "Airlock:".Length);
+                        values = s.Split(',');
+                    }
+                }
+                // split on ;
+
+                // loop the strings
+
+                // split on ,
+
+                // get the id, the name, create an index, check for master, slave
+
+                if (s.Contains("Airlock") && s.Contains("Master"))
+                {
+                    // new airlock code
+                    airlock = new AirlockType();
+                    airlock.id = id; // set the id
+                    airlock.masterVent = blocklist[i] as IMyAirVent;
+                    airlockList.Add(airlock); // add to the list
+                    Echo("Found Airlock " + id.ToString() + ".");
+                }
+            }
+
+            Echo("Completed searching for Airlocks.");
+        }
+
+        public struct DoorType
+        {
+            public int id;
+            public List<IMyDoor> doorList;
+            public List<IMyTextPanel> lcdList;
+            public List<IMySoundBlock> soundList;
+            public List<IMyAirVent> ventList;
+            public List<IMyLightingBlock> lightList;
+            public List<IMySensorBlock> sensorList;
+        }
+
+        public struct AirlockType
+        {
+            public int id;
+            public IMyAirVent masterVent;
+            public List<DoorType> doorList;
+            public List<IMyTextPanel> lcdList;
+            public List<IMySoundBlock> soundList;
+            public List<IMyAirVent> ventList;
+            public List<IMyLightingBlock> lightList;
+            public List<IMySensorBlock> sensorList;
+        }
+    }
+}
---
+++ Dynamic Airlocks/Readme.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *   R e a d m e
+ *   -----------
+ * 
+ *   In this file you can include any instructions or other comments you want to have injected onto the 
+ *   top of your final script. You can safely delete this file if you do not want any such comments.
+ * 
+ * Concepts:
+ * place the word "Airlock" followed by an alphanumeric identifier in the customdata field of each block the airlock is to utilize
+ * 
+ * Pick a "master" air vent for the main configuration of each airlock.
+ * 
+ * O2 Bottles - designate bottles to be controlled by the script
+ * O2 Generators - designate generators to be controlled by the script
+ * LCD Panels - assign LCD's to airlocks (and optionally, to doors)
+ * Air Vents - 
+ * Doors - 
+ * Lights - 
+ * Sound Blocks - 
+ * Sensor Blocks
+ * 
+ * 
+ * Air Vent
+ * Airlock:<id>,master|slave,name;
+ * 
+ * Door
+ * Airlock:<id>,<doorid>,name;
+ * 
+ * Other
+ * Airlock:<id>,<doorid>; (assigned to door, outside)
+ * Airlock:<id>; (assigned to airlock, inside)
+ */
